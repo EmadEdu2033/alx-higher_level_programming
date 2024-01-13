89111d72a7e9fe96843eb3ea8da1c12ddb9c304a
Emad Atef Mohamed Abdel Hamid <131690341+EmadEdu2033@users.noreply.github.com>
2024-01-13 12:57:30 +0200

    2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -5,7 +5,6 @@ def uniq_add(my_list=[]):
     for num in my_list:
         if num not in unique_set:
             unique_set.add(num)
-
     return sum(unique_set)
 my_list = [1, 2, 3, 4, 2, 3, 5]
 result = uniq_add(my_list)
